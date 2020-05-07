84db76b72535c7206c7cd53af8f0a786f0021388
undeadline <ermak321@gmail.com>
2020-05-07 16:41:23 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+JSON Web Token (JWT)
+=========================
+
+This package help you with JWT generating, refreshing, checking.
---
+++ src/JWT.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace undeadline\jwt;
+
+class JWT
+{
+    public static function generate_token($payload)
+    {
+
+    }
+
+    public static function refresh_token()
+    {
+
+    }
+
+    public static function check_token($token)
+    {
+
+    }
+}
